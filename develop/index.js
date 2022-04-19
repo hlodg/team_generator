507cd59495694382c7690d2826f11207ae12f278
Hannah Lodge <lodg0021@umn.edu>
2022-04-19 10:13:10 -0500

    added inquirer

--- develop/index.js
+++ develop/index.js
@@ -1,6 +1,34 @@
-// create packageJSON
 // inquirer for team members
 // inquirer for information of team members
 // create classes for each type of role
 // html file generation - writing file
 
+const inquirer = require("inquirer");
+const fs = require('fs');
+
+const questionsAll= [
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your employee name?',
+    },
+    {
+      type: 'input',
+      name: 'role',
+      message: 'What is thier role?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is thier email?',
+      },
+    {
+        type: 'input',
+        name: 'git',
+        message: 'What is thier github username?',
+    },
+  ]
+
+
+inquirer
+    .prompt(questionsAll)
