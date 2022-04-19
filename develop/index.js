27af6270c36bb0d2a6d64523e5cfe89e6b0338eb
Hannah Lodge <lodg0021@umn.edu>
2022-04-19 10:28:49 -0500

    created inquirer for the different people

--- develop/index.js
+++ develop/index.js
@@ -6,29 +6,147 @@
 const inquirer = require("inquirer");
 const fs = require('fs');
 
-const questionsAll= [
+const role= [
     {
-      type: 'input',
-      name: 'name',
-      message: 'What is your employee name?',
-    },
-    {
-      type: 'input',
+      type: 'list',
       name: 'role',
       message: 'What is thier role?',
-    },
-    {
-        type: 'input',
-        name: 'email',
-        message: 'What is thier email?',
-      },
-    {
-        type: 'input',
-        name: 'git',
-        message: 'What is thier github username?',
-    },
-  ]
+      choices: 'manager, engineer, intern',
+    }
+  ];
 
+
+runNewQuest(answers) {
+    if (answers.role==="manager"){
+        inquirer
+            .prompt([
+                {
+                    type: 'input',
+                    name: 'name',
+                    message: 'What is thier name?',
+                },
+                {
+                    type: 'input',
+                    name: 'git',
+                    message: 'What is thier github username?',
+                },
+                {
+                    type: 'input',
+                    name: 'email',
+                    message: 'What is thier email?',
+                },
+                {
+                    type: 'input',
+                    name: 'office',
+                    message: 'What is thier office number?',
+                },
+                {
+                    type: 'input',
+                    name: 'ID',
+                    message: 'What is thier employee ID?',
+                },
+            ])
+            .then((managerAns) => {
+                return managerAns;
+                })
+    }
+    else if(answers.role==="engineer"){
+     inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is thier name?',
+            },
+            {
+                type: 'input',
+                name: 'git',
+                message: 'What is thier github username?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is thier email?',
+            },
+          
+            {
+                type: 'input',
+                name: 'ID',
+                message: 'What is thier employee ID?',
+            },
+        ])
+        .then((engineerAns) => {
+            return engineerAns;
+            })
+    }else if(answers.role==="engineer"){
+     inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'What is thier name?',
+            },
+            {
+                type: 'input',
+                name: 'git',
+                message: 'What is thier github username?',
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is thier email?',
+            },
+          
+            {
+                type: 'input',
+                name: 'ID',
+                message: 'What is thier employee ID?',
+            },
+        ])
+        .then((engineerAns) => {
+            return engineerAns;
+            })
+    }
+    else if(answers.role==="intern"){
+        inquirer
+           .prompt([
+               {
+                   type: 'input',
+                   name: 'name',
+                   message: 'What is thier name?',
+               },
+               {
+                   type: 'input',
+                   name: 'email',
+                   message: 'What is thier email?',
+               },
+               {
+                   type: 'input',
+                   name: 'ID',
+                   message: 'What is thier student ID?',
+               },
+               {
+                type: 'input',
+                name: 'school',
+                message: 'What is thier sschool?',
+            },
+           ])
+           .then((internAns) => {
+               return internAns;
+               })
+       }
+
+};
 
 inquirer
-    .prompt(questionsAll)
+    .prompt(role)
+    .then((answers) => {
+        return runNewQuest();
+        })
+      .catch((error) => {
+        if (error.isTtyError) {
+            return "There is an error."
+        } else {
+            return "Something else went wrong."
+        }
+      });
